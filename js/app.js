209c85cc73cf93b9689870738511808f3d7c6c02
mchapliuk <m.chapliuk@gmail.com>
2013-07-15 22:06:14 +0300

    app.js - code reformatted, some refactoring. All warnings (except 3) were fixed

--- js/app.js
+++ js/app.js
@@ -21,7 +21,8 @@ var weekHeader = {
     endMonth: '',
     endYear: '',
 
-    init: function() {
+    init: function () {
+        'use strict';
         this.startDayOfWeek = this.startDate.getDay();
         this.startDay = this.startDate.getDate();
         this.startMonth = this.startDate.getMonth();
@@ -33,41 +34,61 @@ var weekHeader = {
         this.endYear = this.endDate.getFullYear();
     },
 
-    getCaption: function() {
+
+    getShortNameOfMonth: function (monthIndex) {
+        'use strict';
+        switch (+monthIndex) {
+            case 0:
+                return 'Jan';
+            case 1:
+                return 'Feb';
+            case 2:
+                return 'Mar';
+            case 3:
+                return 'Apr';
+            case 4:
+                return 'May';
+            case 5:
+                return 'Jun';
+            case 6:
+                return 'Jul';
+            case 7:
+                return 'Aug';
+            case 8:
+                return 'Sep';
+            case 9:
+                return 'Oct';
+            case 10:
+                return 'Nov';
+            case 11:
+                return 'Dec';
+            default :
+                return 'Unk';
+        }
+    },
+
+    getCaption: function () {
         return getShortNameOfDay(this.startDayOfWeek) +
             ', ' + this.startDay + ' ' + this.getShortNameOfMonth(this.startMonth) + ' ' + this.startYear +
             ' - ' + getShortNameOfDay(this.endDayOfWeek) + ', ' + this.endDay + ' ' + this.getShortNameOfMonth(this.endMonth) +
             ' ' + this.endYear;
-    },
-
-    getShortNameOfMonth: function(monthIndex) {
-        switch (+monthIndex) {
-            case 0: return 'Jan';
-            case 1: return 'Feb';
-            case 2: return 'Mar';
-            case 3: return 'Apr';
-            case 4: return 'May';
-            case 5: return 'Jun';
-            case 6: return 'Jul';
-            case 7: return 'Aug';
-            case 8: return 'Sep';
-            case 9: return 'Oct';
-            case 10: return 'Nov';
-            case 11: return 'Dec';
-            default : return 'Unk';
-        }
     }
 };
 
+// Delete this var
 var tableCaptions = ['DoW', 'Issue', 'Time log', 'Select'];
 
 /**
  * Builds a table model with Issues from JIRA
- * @param aData - JSON from JIRA
+ * @param data table model
  */
-function processTableModel(data) {
+function processTableModel (data) {
 
     data = JSON.parse(data);
+
+    if (!(data && data.worklog)) {
+        return;
+    }
 
     tableModel = {};
 
@@ -76,33 +97,43 @@ function processTableModel(data) {
 
     weekHeader.init();
 
-    var tmpItems = [];
-    var totalHours = 0;
+    var tmpItems = [],
+        totalHours = 0,
+        key, issueKey, issueName, innerKey,
+        logDate, timeSpent, item,
+        workLog = data.worklog,
+        workLogEntries;
 
-    for (var key in data.worklog) {
 
+    for (key in workLog) {
+        if (workLog.hasOwnProperty(key)) {
+            issueKey = workLog[key].key;
+            issueName = workLog[key].summary;
+            workLogEntries = workLog[key].entries;
 
-        var issueKey = data.worklog[key].key,
-            issueName = data.worklog[key].summary;
+            for (innerKey in workLogEntries) {
+                if (workLogEntries.hasOwnProperty(innerKey)) {
+                    logDate = new Date(+workLogEntries[innerKey].startDate);
+                    timeSpent = workLogEntries[innerKey].timeSpent / 3600; // time in hours
 
-        for (var innerKey in data.worklog[key].entries) {
-            var logDate = new Date(+data.worklog[key].entries[innerKey].startDate),
-                timeSpent = data.worklog[key].entries[innerKey].timeSpent/3600; // time in hours
+                    item = {};
+                    item.key = logDate.getDay();
+                    item.issueKey = issueKey;
+                    item.issueName = issueName;
+                    item.timeSpent = timeSpent;
 
-            var item = {};
-            item.key = logDate.getDay();
-            item.issueKey = issueKey;
-            item.issueName = issueName;
-            item.timeSpent = timeSpent;
+                    totalHours += timeSpent;
 
-            totalHours += timeSpent;
-
-            tmpItems.push(item);
+                    tmpItems.push(item);
+                }
+            }
         }
     }
 
-    $.each(tmpItems, function(index) {
-        var modelItem = {};
+    var modelItem;
+
+    $.each(tmpItems, function (index) {
+        modelItem = {};
         modelItem.issueKey = tmpItems[index].issueKey;
         modelItem.issueName = tmpItems[index].issueName;
         modelItem.timeSpent = tmpItems[index].timeSpent;
@@ -123,12 +154,12 @@ function processTableModel(data) {
  * @param tm - table model
  * @param tc - array of table captions
  */
-function drawAndFillTable(tm, tc) {
+function drawAndFillTable (tm, tc) {
 
-    var timeSpentSummary = 0;
+    var tableContent = $('#tableContent');
 
     if (document.getElementById('jiraLog')) {
-        $('#tableContent').empty();
+        tableContent.empty();
     }
 
     var table = $('<table/>').addClass('table no-margin-bottom table-hover table-condensed').attr('id', 'jiraLog');
@@ -140,31 +171,39 @@ function drawAndFillTable(tm, tc) {
     var tableHeader = $('<thead/>'),
         tableHeaderRow = $('<tr/>');
 
-    $.each(tc, function(i) {
-        tableHeaderRow.append('<th>'+ tc[i] +'</th>');
+    $.each(tc, function (i) {
+        tableHeaderRow.append('<th>' + tc[i] + '</th>');
     });
 
     tableHeader.append(tableHeaderRow);
     table.append(tableHeader);
 
-    var tableBody = $('<tbody/>');
+    var tableBody = $('<tbody/>'),
+        key,
+        dayIndex,
+        dow,
+        rowSpan,
+        row,
+        dataRow;
 
     // Fill table with data
-    for (var key in tm) {
-        var dayIndex = key;
-        var dow = getShortNameOfDay(dayIndex);
-        var rowspan = tm[key].length;
+    for (key in tm) {
+        if (tm.hasOwnProperty(key)) {
+            dayIndex = key;
+            dow = getShortNameOfDay(dayIndex);
+            rowSpan = tm[key].length;
 
-        $.each(tm[key], function(i) {
-            var dataRow = $('<tr/>');
-            if (i === 0) {
-                var row = $('<td rowspan="' + rowspan + '" class="middle-cell">' + dow + '</td><td class="issueLink" data-key="'+this.issueKey+'">' + this.issueKey + ' - ' + this.issueName + '</td><td class="middle-cell">' + this.timeSpent + ' h</td><td rowspan="' + rowspan + '" class="middle-cell"><input type="checkbox" name="sel" value="' + dayIndex + '" checked/></td>');
-            } else {
-                var row = $('<td class="issueLink" data-key="'+this.issueKey+'">' + this.issueKey + ' - ' + this.issueName + '</td><td class="middle-cell">' + this.timeSpent + ' h</td>');
-            }
-            dataRow.append(row);
-            tableBody.append(dataRow);
-        });
+            $.each(tm[key], function (i) {
+                dataRow = $('<tr/>');
+                if (i === 0) {
+                    row = $('<td rowspan="' + rowSpan + '" class="middle-cell">' + dow + '</td><td class="issueLink" data-key="' + this.issueKey + '">' + this.issueKey + ' - ' + this.issueName + '</td><td class="middle-cell">' + this.timeSpent + ' h</td><td rowspan="' + rowSpan + '" class="middle-cell"><input type="checkbox" name="sel" value="' + dayIndex + '" checked/></td>');
+                } else {
+                    row = $('<td class="issueLink" data-key="' + this.issueKey + '">' + this.issueKey + ' - ' + this.issueName + '</td><td class="middle-cell">' + this.timeSpent + ' h</td>');
+                }
+                dataRow.append(row);
+                tableBody.append(dataRow);
+            });
+        }
     }
 
     table.append(tableBody);
@@ -173,17 +212,7 @@ function drawAndFillTable(tm, tc) {
     table.append(tableFooter);
 
 
-    $('#tableContent').append(table);
-
-//    $('#fillWebtime').click(function (e) {
-//        var dataModel = prepareModelToSend(tableModel);
-//
-//        chrome.tabs.getSelected(null, function (tab) {
-//            chrome.tabs.sendMessage(tab.id, {channel: 'fillWebtimeButtonClicked', dataModel: dataModel}, function (response) {
-//                console.log(response.farewell);
-//            });
-//        });
-//    });
+    tableContent.append(table);
 }
 
 
@@ -192,27 +221,35 @@ function drawAndFillTable(tm, tc) {
  * @param tm - table model
  * @returns {{new table model without unchecked days}}
  */
-function prepareModelToSend(tm) {
+function prepareModelToSend (tm) {
     var modelToSend = {};
     var checkedDays = $("input[name='sel']:checked");
 
-    $.each(checkedDays, function() {
-        modelToSend[$(this).val()] =  tm[$(this).val()];
+    $.each(checkedDays, function () {
+        modelToSend[$(this).val()] = tm[$(this).val()];
     });
 
     return modelToSend;
 }
 
-function getShortNameOfDay(dayIndex) {
+function getShortNameOfDay (dayIndex) {
     switch (+dayIndex) {
-        case 0: return 'Sun';
-        case 1: return 'Mon';
-        case 2: return 'Tue';
-        case 3: return 'Wed';
-        case 4: return 'Thu';
-        case 5: return 'Fri';
-        case 6: return 'Sat';
-        default : return 'Unk';
+        case 0:
+            return 'Sun';
+        case 1:
+            return 'Mon';
+        case 2:
+            return 'Tue';
+        case 3:
+            return 'Wed';
+        case 4:
+            return 'Thu';
+        case 5:
+            return 'Fri';
+        case 6:
+            return 'Sat';
+        default :
+            return 'Unk';
     }
 }
 
