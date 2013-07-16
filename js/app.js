e92286b91b706352bca32eea26b186414352eeaf
mchapliuk <m.chapliuk@gmail.com>
2013-07-16 16:59:09 +0300

    app.js - getShortNameOfDay and getShortNameOfMonth have been changed;

--- js/app.js
+++ js/app.js
@@ -8,59 +8,28 @@
 
 function getShortNameOfDay (dayIndex) {
     'use strict';
-
-    switch (+dayIndex) {
-        case 0:
-            return 'Sun';
-        case 1:
-            return 'Mon';
-        case 2:
-            return 'Tue';
-        case 3:
-            return 'Wed';
-        case 4:
-            return 'Thu';
-        case 5:
-            return 'Fri';
-        case 6:
-            return 'Sat';
-        default :
-            return 'Unk';
+    var result = 'Unk';
+    dayIndex = +dayIndex;
+    if (dayIndex >= 0 && dayIndex <= 6) {
+        var daysNames = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+        result = daysNames[dayIndex];
     }
+
+    return result;
 }
 
 function TableModel (data) {
     "use strict";
 
     this._getShortNameOfMonth = function (monthIndex) {
-        switch (+monthIndex) {
-            case 0:
-                return 'Jan';
-            case 1:
-                return 'Feb';
-            case 2:
-                return 'Mar';
-            case 3:
-                return 'Apr';
-            case 4:
-                return 'May';
-            case 5:
-                return 'Jun';
-            case 6:
-                return 'Jul';
-            case 7:
-                return 'Aug';
-            case 8:
-                return 'Sep';
-            case 9:
-                return 'Oct';
-            case 10:
-                return 'Nov';
-            case 11:
-                return 'Dec';
-            default :
-                return 'Unk';
+        var result = 'Unk';
+        monthIndex = +monthIndex;
+        if (monthIndex >= 0 && monthIndex <= 11) {
+            var monthsNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+            result = monthsNames[monthIndex];
         }
+
+        return result;
     };
 
     this._datesInit = function () {
